607e96788e102b9938a6ec463d423539b005a15b
mendamar0517 <mend2023@gmail.com>
2025-03-17 12:31:46 +0800

    Lab 7: inheritance болон composition шийдлүүд

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -1,16 +1,54 @@
 package edu.cmu.cs.cs214.rec04;
 
-/**
- * DelegationSortedIntList -- a variant of a SortedIntList that keeps
- * count of the number of attempted element insertions (not to be confused
- * with the current size, which goes down when an element is removed)
- * and exports an accessor (totalAdded) for this count.
- *
- * @author Nora Shoemaker
- *
- */
+public class DelegationSortedIntList implements IntegerList {
+    private final SortedIntList list = new SortedIntList();
+    private int totalAdded = 0;
 
-public class DelegationSortedIntList {
-    // Write your implementation below with API documentation
+    @Override
+    public boolean add(int value) {
+        totalAdded++;
+        return list.add(value);
+    }
 
+    @Override
+    public boolean addAll(IntegerList values) {
+        totalAdded += values.size();
+        // IntegerList дахь addAll методыг тодорхойлох
+        if (values instanceof DelegationSortedIntList delegationSortedIntList) {
+            return list.addAll(delegationSortedIntList.getList());
+        }
+        // Мөн бусад IntegerList хэсэгт хэрэгжүүлж болно
+        return false;
+    }
+
+    @Override
+    public boolean remove(int value) {
+        return list.remove(value);
+    }
+
+    @Override
+    public boolean removeAll(IntegerList values) {
+        if (values instanceof DelegationSortedIntList delegationSortedIntList) {
+            return list.removeAll(delegationSortedIntList.getList());
+        }
+        return false;
+    }
+
+    @Override
+    public int get(int index) {
+        return list.get(index);
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    public int getTotalAdded() {
+        return totalAdded;
+    }
+
+    public SortedIntList getList() {
+        return list;
+    }
 }
--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -1,16 +1,25 @@
 package edu.cmu.cs.cs214.rec04;
 
-/**
- * InheritanceSortedIntList -- a variant of a SortedIntList that keeps
- * count of the number of attempted element insertions (not to be confused
- * with the current size, which goes down when an element is removed)
- * and exports an accessor (totalAdded) for this count.
- *
- * @author Nora Shoemaker
- *
- */
+public class InheritanceSortedIntList extends SortedIntList {
+    private int totalAdded = 0;
 
-public class InheritanceSortedIntList {
-    // Write your implementation below with API documentation
+    @Override
+    public boolean add(int value) {
+        totalAdded++;
+        return super.add(value);
+    }
 
+    @Override
+public boolean addAll(IntegerList values) {
+    int initialSize = this.size();  // Одоогийн жагсаалтын хэмжээг хадгалах
+    boolean changed = super.addAll(values);  // Эцэг классын addAll() дуудна
+    int newSize = this.size();  // Шинэ хэмжээг авах
+
+    totalAdded += (newSize - initialSize);  // Нэмэгдсэн элементүүдийн тоогоор шинэчлэх
+    return changed;
+}
+
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
